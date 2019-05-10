9aeb476a4d9da62b64a5208d66de88dfdaf94d4f
jdgaudillo <joverlyn.gaudillo@digenomix.com>
2019-05-10 17:02:12 +0800

    Modify blob.py

--- helper_functions/blob.py
+++ helper_functions/blob.py
@@ -18,6 +18,7 @@ def downloadBlob(account_name, account_key, container, data_dir, blob_name):
 	block_blob_service.get_blob_to_path(container, blob_name, downloaded_file)
 
 	print("Successfully downloaded ", downloaded_file.split("/")[-1])
+	print("\n")
 
 	return downloaded_file
 
@@ -25,12 +26,24 @@ def downloadBlob(account_name, account_key, container, data_dir, blob_name):
 def uploadBlob(account_name, account_key, container, local_paths):
 	block_blob_service = BlockBlobService(account_name=account_name, account_key=account_key)
 
-	blob_files = [local_path.split("/")[-1] for local_path in local_paths]
-	blob_paths = [os.path.join("dynamic", blob_file) for blob_file in blob_files]
+	if len(local_paths) == 1:
+		blob_files = local_paths.split("/")[-1]
+		blob_paths = os.path.join("dynamic", blob_files)
 
-	for i, blob_path in enumerate(blob_paths):
-		print("Uploading {}".format(blob_path))
+		print("Uploading {}".format(blob_paths))
 		block_blob_service.create_blob_from_path(container, blob_path, local_paths[i])
+
+	else:
+		blob_files = [local_path.split("/")[-1] for local_path in local_paths]
+		blob_paths = [os.path.join("dynamic", blob_file) for blob_file in blob_files]
+
+		for i, blob_path in enumerate(blob_paths):
+			print("Uploading {}".format(blob_path))
+			block_blob_service.create_blob_from_path(container, blob_path, local_paths[i])
+
+
+def copyBlob(source, dest):
+	shutil.copy(source, dest)
 
 
 def extractBlob(source, dest):
@@ -38,17 +51,12 @@ def extractBlob(source, dest):
 		filenames = zipObj.namelist()
 
 		extract_file = source.split("/")[-1][:-4] + "/results.csv"
-		print(extract_file)
 
 		print("Extracting file.................")
 		zipObj.extract(extract_file, dest)
 		print("Successfully extracted file!")
-		print("==============EXTRACT BLOB====================")
+		print("==============EXTRACT BLOB====================\n")
 
-		"""for name in filenames:
-									if name.endswith(".csv"):
-										print("Extracting file.....")
-						
-										zipObj.extract(name, dest)
-						
-										print("Successfully extracted ", name.split("/")[-1], "to ", dest)"""
+	extract_file = os.path.join(dest, extract_file)
+	dest = os.path.abspath("../Blob-Containers/unprocessed")
+	copyBlob(extract_file, dest)
