2c50b37148dc2f07dad5b0bcafb6ac8395d91149
jdgaudillo <joverlyn.gaudillo@digenomix.com>
2019-05-13 08:44:50 +0800

    Add columns to voter turnout live

--- anomaly_checking/voter_turnout_live.py
+++ anomaly_checking/voter_turnout_live.py
@@ -30,7 +30,7 @@ def preprocessData(data):
 	col_type = {"VCM_ID": str}
 
 	precincts = readCSV(precincts_file, col_type, ["VCM_ID", "REG_NAME", "PRV_NAME", "MUN_NAME", "REGISTERED_VOTERS"])
-	
+
 	data = data.drop_duplicates(subset=["PRECINCT_CODE"])
 
 	data = data.merge(precincts, left_on = 'PRECINCT_CODE', right_on = 'VCM_ID', how = 'left')
@@ -39,18 +39,55 @@ def preprocessData(data):
 
 	return data
 
+def addVoterTurnout(df):
 
-def addVoterTurnout(data):
+	df.loc[:, "PERCENT_VOTER_TURNOUT"] = (df.NUMBER_VOTERS / df.REGISTERED_VOTERS) * 100.
 
-	merged_data = preprocessData(data)
+	voter_turnout = df.groupby(["REG_NAME", "PRV_NAME", "MUN_NAME"], sort=False)["NUMBER_VOTERS", "REGISTERED_VOTERS"].sum().reset_index()
+
+	voter_turnout.loc[:, "PERCENT_VOTER_TURNOUT"] = (voter_turnout.NUMBER_VOTERS / voter_turnout.REGISTERED_VOTERS) * 100.
+
+	return voter_turnout
+
+
+def addTransmittedPrecincts(df):
+
+	data_dir = os.path.join(blob_directory, "processed/static")
+	total_precincts_file = os.path.join(data_dir, "total_precincts.csv")
 	
-	merged_data.loc[:, "PERCENT_VOTER_TURNOUT"] = (merged_data.NUMBER_VOTERS / merged_data.REGISTERED_VOTERS) * 100.
+	total_precincts = pd.read_csv(total_precincts_file, sep=",", encoding="utf-8")
+	total_precincts = total_precincts.set_index("try_key")
 
-	merged_data = merged_data.drop(["VCM_ID", "PRECINCT_CODE"], axis=1)
-
-	mun_group = merged_data.groupby(["REG_NAME", "PRV_NAME", "MUN_NAME"], sort=False)["PERCENT_VOTER_TURNOUT"].mean().to_frame().reset_index()
+	df.loc[:, "NUMBER_TRANSMITTED_PRECINCTS"] = 1.
 	
-	return mun_group
+	transmitted_precincts = df.groupby(["REG_NAME", "PRV_NAME", "MUN_NAME"], sort=False)["NUMBER_TRANSMITTED_PRECINCTS"].sum().to_frame().reset_index()
+	transmitted_precincts.loc[:, "try_key"] = transmitted_precincts.PRV_NAME + "-" + transmitted_precincts.MUN_NAME
+	transmitted_precincts = transmitted_precincts.set_index("try_key")
+
+	res = pd.concat([transmitted_precincts, total_precincts], axis=1, join_axes=[transmitted_precincts.index])
+
+	res.columns = ['REG_NAME', 'PRV_NAME', 'MUN_NAME', 'NUMBER_TRANSMITTED_PRECINCTS', 
+					'REG_NAME_x', 'PRV_NAME_x', 'MUN_NAME_x', 'TOTAL_PRECINCTS']
+			
+	res = res.drop(["REG_NAME", "PRV_NAME", "MUN_NAME", "REG_NAME_x", "PRV_NAME_x", "MUN_NAME_x"], axis=1)
+
+	res.loc[:, "PERCENT_TRANSMITTED_PRECINCTS"] = (res.NUMBER_TRANSMITTED_PRECINCTS/res.TOTAL_PRECINCTS) * 100.
+
+	return res
+
+
+def addColumns(data):
+	
+	voter_turnout = addVoterTurnout(data)
+	voter_turnout.loc[:, "try_key"] = voter_turnout.PRV_NAME + "-" + voter_turnout.MUN_NAME
+	voter_turnout = voter_turnout.set_index("try_key")
+
+	transmitted_precincts = addTransmittedPrecincts(data)
+	
+	res = pd.concat([voter_turnout, transmitted_precincts], axis=1, join_axes=[voter_turnout.index])
+	res = res.reset_index(drop=True)
+
+	return res
 
 
 def logVoterTurnout(data, order):
@@ -60,7 +97,7 @@ def logVoterTurnout(data, order):
 	with open(log_file, "w+", encoding="utf-8") as f:
 		data.to_csv(f, index=False, encoding="utf-8")
 
-	print("Successfully added log ", log_file.split("/")[-1])
+	print("\nSuccessfully added log ", log_file.split("/")[-1])
 
 
 if __name__ == '__main__':
@@ -91,12 +128,15 @@ if __name__ == '__main__':
 
 	results = readCSV(results_file, col_type, cols)
 
-	voter_turnout = addVoterTurnout(results.loc[old_shape[0]:])
+	merged_results = preprocessData(results)
+
+	final_df = addColumns(merged_results)
+
+	print("Printing first 5 rows in the log file. \n")
+	print(final_df.head())
 			
-	logVoterTurnout(voter_turnout, order)
+	logVoterTurnout(final_df, order)
 
 	print("\nTOTAL RUNTIME: ", time.time() - start_time)
 
 	print("\n============END OF VOTER TURNOUT LIVE===================\n")
-
-
